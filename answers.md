0deda2acdc3b5a36655ed8ffdf492e5ffab57498
fairbanksdp <fairbanksdp@s.dcsdk12.org>
2017-01-31 12:31:18 -0700

    Finish activity 2

--- answers.md
+++ answers.md
@@ -5,4 +5,6 @@ I said no! -> Why so negative?
 The weather is nice. -> Do you really think so?
 Do you know my brother? -> Why so negative?
 
+when a keyword is included in another word it is still counted
+the firs example trigers the negative response and the second one triggers the family response
 
--- src/Magpie2.java
+++ src/Magpie2.java
@@ -31,17 +31,35 @@ public class Magpie2 {
 	 */
 	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0) {
+		if (strEmpty(statement)) 
+			response = "please enter valid statement";
+		else if (statement.indexOf("no") >= 0) 
 			response = "Why so negative?";
-		} else if (statement.indexOf("mother") >= 0
+		else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0) {
+				|| statement.indexOf("brother") >= 0) 
 			response = "Tell me more about your family.";
-		} else {
+		else if (statement.indexOf("dog") >= 0 
+				|| statement.indexOf("cat") >= 0) 
+			response = "Tell me more about your pets";
+		else if (statement.indexOf("Mr. Padjen") >= 0) 
+			response = "He sounds like a good teacher.";
+		else if (statement.indexOf("so") >= 0)
+			response = "So what?";
+		else if (statement.indexOf("Why") >= 0
+				|| statement.indexOf("?") >= 0)
+			response = "I ask myself that question every day.";
+		else 
 			response = getRandomResponse();
-		}
 		return response;
+	}
+
+	public boolean strEmpty(String str) {
+		//str.trim();
+		if ("".equals(str.trim()))
+			return true;
+		return false;
 	}
 
 	/**
@@ -50,7 +68,7 @@ public class Magpie2 {
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 7;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -63,6 +81,12 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "Why don't we change the subject.";
+		} else if (whichResponse == 5) {
+			response = "I have ethical objections to this.";
+		} else if (whichResponse == 6) {
+			response = "Oh it all makes sense now.";	
 		}
 
 		return response;
